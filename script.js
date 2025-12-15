3ce1abde690fdaa64e02d3c515eff7604ef38e63
JSJ0512 <ball5854@naver.com>
2025-12-15 17:34:53 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+const links = document.querySelectorAll("nav a");
+
+links.forEach(link => {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const id = this.getAttribute("href").substring(1);
+    document.getElementById(id).scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+});
+
+// 소개 타이핑 효과
+const text = "W E L C O M E !";
+const typingTarget = document.getElementById("typing");
+
+let index = 0;
+let isDeleting = false;
+
+setInterval(function () {
+  if (!isDeleting) {
+    typingTarget.innerText = text.substring(0, index + 1);
+    index++;
+
+    if (index === text.length) {
+      isDeleting = true;
+    }
+  } else {
+    typingTarget.innerText = text.substring(0, index - 1);
+    index--;
+
+    if (index === 0) {
+      isDeleting = false;
+    }
+  }
+}, 200);
